06ba80df1da1d03f2fa5d029695ba2bcf21ea11d
XINGGou <1546602112@qq.com>
2021-02-18 10:44:05 +0800

    创建vue_day07

---
+++ 初学Vue代码/vue_day07/src/main/java/com/xxh/vue_day07/VueDay07Application.java
@@ -0,0 +1,13 @@
+package com.xxh.vue_day07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueDay07Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueDay07Application.class, args);
+    }
+
+}
---
+++ 初学Vue代码/vue_day07/src/test/java/com/xxh/vue_day07/VueDay07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxh.vue_day07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueDay07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
